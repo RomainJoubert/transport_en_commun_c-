8f17738e6e5d377c7c265fb5903c774f4da1b591
RomainJoubert <romain.joubert@Digital-Grenoble.local>
2018-08-23 15:21:15 +0200

    lecture api et suppression des doublons

---
+++ PublicTransport/Program.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PublicTransport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+
+            String longitude = "5.726744267129334";
+            String latitude = "45.18521853612248";
+            Int32 distance = 600;
+
+
+
+            // Create a request for the URL.   
+            WebRequest request = WebRequest.Create(
+                  "http://data.metromobilite.fr/api/linesNear/json?x=" + longitude + "&y=" + latitude + "&dist=" + distance + "&details=true");
+            // If required by the server, set the credentials.  
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response.  
+            WebResponse response = request.GetResponse();
+            // Display the status.  
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.  
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.  
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.  
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.  
+            //Console.WriteLine(responseFromServer);
+            
+            List<BusStationObject> busStation = JsonConvert.DeserializeObject<List<BusStationObject>>(responseFromServer);
+            //cette ligne convertit la réponse qui est au format json en collection d'objets C#
+
+            Dictionary<string, List<string>> leilaReveilleToi =
+                new Dictionary<string, List<string>>();
+
+            // Clean up the streams and the response.  
+            reader.Close();
+            response.Close();
+            dataStream.Close();
+            
+        }
+       
+
+    }
+}
---
+++ PublicTransport/Toolbox.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicTransport
+{
+    class Toolbox
+    {
+    //    public Dictionary RemoveDuplicate(Object test)
+    //    {
+    //        foreach (BusStationObject station in busStation)
+
+    //        {
+    //            try
+    //            {
+    //                if (!leilaReveilleToi.ContainsKey(station.name))
+    //                {
+    //                    leilaReveilleToi.Add(station.name, station.lines);
+    //                }
+    //                else
+    //                {
+    //                    Console.WriteLine("Nb lines : " + station.lines.Count);
+    //                    foreach (string line in station.lines)
+    //                    {
+    //                        if (!leilaReveilleToi[station.name].Contains(line))
+    //                        {
+    //                            leilaReveilleToi[station.name].Add(line);
+    //                        }
+    //                    }
+    //                }
+    //            }
+    //            catch (ArgumentException)
+    //            {
+    //                Console.WriteLine("An element with Key = " + station.name + " already exists.");
+    //            }
+
+    //        }
+
+    //        //affichage du dictionnaire
+    //        foreach (KeyValuePair<string, List<string>> kvp in leilaReveilleToi)
+    //        {
+    //            Console.WriteLine("Arret = " + kvp.Key);
+    //            foreach (string line in kvp.Value)
+    //            {
+    //                Console.WriteLine("      Ligne = " + line);
+    //            }
+    //            return Dictionary;
+    //        }
+    //    }
+    }
+}
